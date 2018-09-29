9ee30b6057cd26063f66295902f25272be61b76e
Alice Hsiao <alice.hsiao87@gmail.com>
2018-09-28 17:24:01 -1000

    completed all required parts of rails zoo

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,57 @@
 class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all.order(:age)
+  end
+
+  def show
+    @animal = Animal.find_by(id: params[:id])
+
+    if @animal.nil?
+      render :notfound, status: :not_found
+    end
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    animal = Animal.new(animal_params)
+
+    if animal.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    animal = Animal.find(params[:id])
+    animal.update(animal_params)
+
+    if animal.save
+      redirect_to animal_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    animal = Animal.find_by(id: params[:id])
+
+    if animal.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
+  private
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'animals#index'
+
+  get '/animals/new', to: 'animals#new', as: 'new_animal'
+  post '/animals', to: 'animals#create'
+
+
+  get '/animals', to: 'animals#index'
+  get '/animals/:id', to: 'animals#show', as: 'animal'
+
+  get '/animals/:id/edit', to: 'animals#edit', as: 'edit_animal'
+  patch '/animals/:id', to: 'animals#update'
+
+  delete '/animals/:id', to: 'animals#destroy'
 end
